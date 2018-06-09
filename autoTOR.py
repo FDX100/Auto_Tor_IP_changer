543d45f0eb93cbb4f87ded69ed2a116c400d07f6
FDX100 <40120613+FDX100@users.noreply.github.com>
2018-06-09 10:32:40 -0400

    Add files via upload

---
+++ autoTOR.py
@@ -0,0 +1,76 @@
+import time
+import os
+import requests
+os.system("clear")
+
+print('''\033[1;32;40m \n
+
+___  __   __        __      __                  __   ___  __      __          ___  __  
+ |  /  \ |__)    | |__)    /  ` |__|  /\  |\ | / _` |__  |__)    |__) \ /    |__  |  \ 
+ |  \__/ |  \    | |       \__, |  | /~~\ | \| \__> |___ |  \    |__)  |     |    |__/ 
+                                                                                       
+
+
+
+
+''')
+print("\033[1;40;31m *if you dont have tor . then you must install tor (sudo apt-get install tor)\n")
+os.system("service tor start")
+
+
+time.sleep(5)
+print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
+x = input("time to change Ip in Sec ")
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+time.sleep(x)
+os.system("service tor reload")
+print("Your IP has been Changed  :") 
+
+
+
+
+
