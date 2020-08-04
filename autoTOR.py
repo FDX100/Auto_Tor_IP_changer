fa02d28a558da7e28d1115526d5b5d338dd2807a
FDX100 <40120613+FDX100@users.noreply.github.com>
2020-08-04 14:17:59 +0300

    autoTOR V2.1
    bug fixed
    work on python 3

--- autoTOR.py
+++ autoTOR.py
@@ -2,53 +2,73 @@
 
 import time
 import os
-import requests
+import subprocess
+
+
+
+
+try:
+    check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
+    if str('install ok installed') in str(check_pip3):
+        pass
+except subprocess.CalledProcessError:
+    print('[+] pip3 not installed')
+    subprocess.check_output('sudo apt update',shell=True)
+    subprocess.check_output('sudo apt install python3-pip -y', shell=True)
+    print('[!] pip3 installed succesfully')
+
+
+
+try:
+
+    import requests
+except Exception:
+    print('[+] python3 requests is not installed')
+    os.system('pip3 install requests')
+    print('[!] python3 requests is installed ')
+try:
+
+    check_tor = subprocess.check_output('which tor', shell=True)
+except subprocess.CalledProcessError:
+
+    print('[+] tor is not installed !')
+    subprocess.check_output('sudo apt update',shell=True)
+    subprocess.check_output('sudo apt install tor -y',shell=True)
+    print('[!] tor is installed succesfully ')
+
 os.system("clear")
 def ma_ip():
     url='https://www.myexternalip.com/raw'
-    get_ip= requests.get(url,proxies=dict(http='socks5://127.0.0.1:9050',
-                                 https='socks5://127.0.0.1:9050'))
+    get_ip= requests.get(url,proxies=dict(http='socks5://127.0.0.1:9050',https='socks5://127.0.0.1:9050'))
     return get_ip.text
 
 def change():
     os.system("service tor reload")
-    print ('<<<<<<<<<<<<<<<<Your IP has been Changed>>>>>>>>>>>>>>>>> '+str(ma_ip()))
+    print ('[+] Your IP has been Changed to : '+str(ma_ip()))
 
 print('''\033[1;32;40m \n
-
-
- ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄        ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄       ▄▄▄▄▄▄▄▄▄▄   ▄         ▄       ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄
-▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░▌      ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░▌ ▐░▌       ▐░▌     ▐░░░░░░░░░░░▌▐░░░░░░░░░░▌
- ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌      ▀▀▀▀█░█▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌░▌     ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌     ▐░█▀▀▀▀▀▀▀█░▌▐░▌       ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀█░▌
-     ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌          ▐░▌     ▐░▌       ▐░▌     ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌▐░▌    ▐░▌▐░▌          ▐░▌          ▐░▌       ▐░▌     ▐░▌       ▐░▌▐░▌       ▐░▌     ▐░▌          ▐░▌       ▐░▌
-     ▐░▌     ▐░▌       ▐░▌▐░█▄▄▄▄▄▄▄█░▌          ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌     ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌▐░▌ ▐░▌   ▐░▌▐░▌ ▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄▄▄ ▐░█▄▄▄▄▄▄▄█░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄█░▌     ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌
-     ▐░▌     ▐░▌       ▐░▌▐░░░░░░░░░░░▌          ▐░▌     ▐░░░░░░░░░░░▌     ▐░▌          ▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌  ▐░▌  ▐░▌▐░▌▐░░░░░░░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌     ▐░░░░░░░░░░▌ ▐░░░░░░░░░░░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌
-     ▐░▌     ▐░▌       ▐░▌▐░█▀▀▀▀█░█▀▀           ▐░▌     ▐░█▀▀▀▀▀▀▀▀▀      ▐░▌          ▐░█▀▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀█░▌▐░▌   ▐░▌ ▐░▌▐░▌ ▀▀▀▀▀▀█░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀█░█▀▀      ▐░█▀▀▀▀▀▀▀█░▌ ▀▀▀▀█░█▀▀▀▀      ▐░█▀▀▀▀▀▀▀▀▀ ▐░▌       ▐░▌
-     ▐░▌     ▐░▌       ▐░▌▐░▌     ▐░▌            ▐░▌     ▐░▌               ▐░▌          ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌    ▐░▌▐░▌▐░▌       ▐░▌▐░▌          ▐░▌     ▐░▌       ▐░▌       ▐░▌     ▐░▌          ▐░▌          ▐░▌       ▐░▌
-     ▐░▌     ▐░█▄▄▄▄▄▄▄█░▌▐░▌      ▐░▌       ▄▄▄▄█░█▄▄▄▄ ▐░▌               ▐░█▄▄▄▄▄▄▄▄▄ ▐░▌       ▐░▌▐░▌       ▐░▌▐░▌     ▐░▐░▌▐░█▄▄▄▄▄▄▄█░▌▐░█▄▄▄▄▄▄▄▄▄ ▐░▌      ▐░▌      ▐░█▄▄▄▄▄▄▄█░▌     ▐░▌          ▐░▌          ▐░█▄▄▄▄▄▄▄█░▌
-     ▐░▌     ▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░░░░░░░░░░░▌▐░▌               ▐░░░░░░░░░░░▌▐░▌       ▐░▌▐░▌       ▐░▌▐░▌      ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌▐░▌       ▐░▌     ▐░░░░░░░░░░▌      ▐░▌          ▐░▌          ▐░░░░░░░░░░▌
-      ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀       ▀▀▀▀▀▀▀▀▀▀▀  ▀                 ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀  ▀         ▀  ▀        ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀         ▀       ▀▀▀▀▀▀▀▀▀▀        ▀            ▀            ▀▀▀▀▀▀▀▀▀▀
-
-
-
-
-
-
+                _          _______
+     /\        | |        |__   __|
+    /  \  _   _| |_ ___      | | ___  _ __
+   / /\ \| | | | __/ _ \     | |/ _ \| '__|
+  / ____ \ |_| | || (_) |    | | (_) | |
+ /_/    \_\__,_|\__\___/     |_|\___/|_|
+                V 2.1
+from mrFD
 ''')
 print("\033[1;40;31m http://facebook.com/ninja.hackerz.kurdish/\n")
-print("\033[1;40;31m *if you dont have tor . then you must install tor (sudo apt-get install tor , sudo apt-get install privoxy)\n")
 
 os.system("service tor start")
 
 
 
 
-time.sleep(5)
+time.sleep(3)
 print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
 os.system("service tor start")
-x = input("time to change Ip in Sec [type=60] >> ")
-lin = input("how many time do you want to change your ip [type=1000]for infinte ip change type [0] >>")
-if str(lin) =='0':
+x = input("[+] time to change Ip in Sec [type=60] >> ")
+lin = input("[+] how many time do you want to change your ip [type=1000]for infinte ip change type [0] >>")
+if int(lin) ==int(0):
 
 	while True:
 		try:
@@ -56,12 +76,10 @@ if str(lin) =='0':
 			change()
 		except KeyboardInterrupt:
 
-		 	print('auto tor is closed ')
+		 	print('\nauto tor is closed ')
 		 	quit()
 
 else:
-
-
 	for i in range(int(lin)):
 		    time.sleep(int(x))
 		    change()
