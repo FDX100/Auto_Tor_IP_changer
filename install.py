201a10547543b4ac135081977b4374e77d95a91d
satheshsundhar <ssundhar@torrid.com>
2024-07-25 15:01:30 +0530

    changed codes according to mobile

--- install.py
+++ install.py
@@ -1,18 +1,25 @@
 import os
+
 choice = input('[+] to install press (Y) to uninstall press (N) >> ')
 run = os.system
-if str(choice) =='Y' or str(choice)=='y':
+
+if str(choice) == 'Y' or str(choice) == 'y':
 
     run('chmod 777 autoTOR.py')
-    run('mkdir /usr/share/aut')
-    run('cp autoTOR.py /usr/share/aut/autoTOR.py')
+    run('mkdir -p $PREFIX/share/aut')
+    run('cp autoTOR.py $PREFIX/share/aut/autoTOR.py')
 
-    cmnd=(' #! /bin/sh \n exec python3 /usr/share/aut/autoTOR.py "$@"')
-    with open('/usr/bin/aut','w')as file:
+    cmnd = '#! /bin/sh\nexec python3 $PREFIX/share/aut/autoTOR.py "$@"'
+    with open(os.path.join(os.getenv('PREFIX'), 'bin', 'aut'), 'w') as file:
         file.write(cmnd)
-    run('chmod +x /usr/bin/aut & chmod +x /usr/share/aut/autoTOR.py')
-    print('''\n\ncongratulation auto Tor Ip Changer is installed successfully \nfrom now just type \x1b[6;30;42maut\x1b[0m in terminal ''')
-if str(choice)=='N' or str(choice)=='n':
-    run('rm -r /usr/share/aut ')
-    run('rm /usr/bin/aut ')
-    print('[!] now Auto Tor Ip changer  has been removed successfully')
+    
+    run('chmod +x $PREFIX/bin/aut')
+    run('chmod +x $PREFIX/share/aut/autoTOR.py')
+    
+    print('''\n\nCongratulations! Auto Tor IP Changer is installed successfully.\nFrom now on, just type \x1b[6;30;42maut\x1b[0m in the terminal.''')
+
+elif str(choice) == 'N' or str(choice) == 'n':
+    run('rm -rf $PREFIX/share/aut')
+    run('rm $PREFIX/bin/aut')
+    
+    print('[!] Auto Tor IP Changer has been removed successfully.')
